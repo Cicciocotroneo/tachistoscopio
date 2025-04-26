335b6b9c0e011a8a7e1ac3e29c4c6d7d11770160
Cicciocotroneo <74688471+Cicciocotroneo@users.noreply.github.com>
2025-04-26 12:08:52 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,15 @@
+const CACHE_NAME = 'tachistoscopio-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
